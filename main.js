a8b963e44ae4a666b475e0913025551712101b79
antonio <ammj24@hotmail.com>
2019-11-05 18:15:27 +0000

    fetch json data

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 
+d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json')
+    .then(result => {
+        tDiagram(result);
+    });
 
-d3.select('body')
-    .append('h2')
-    .attr('id', 'title')
-    .text('Title')
 
-d3.select('body')
-    .append('h3')
-    .attr('id', 'description')
-    .text('Description')
+function tDiagram(dataset) {
+
+    console.log(dataset)
+
+    d3.select('body')
+        .append('h2')
+        .attr('id', 'title')
+        .text('Title')
+
+    d3.select('body')
+        .append('h3')
+        .attr('id', 'description')
+        .text('Description')
+
+
+
+    
+
+}
+
