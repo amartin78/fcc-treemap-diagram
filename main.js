822333a59aaf394481c87c9e0052d0c329e9c22d
antonio <ammj24@hotmail.com>
2019-11-07 21:08:33 +0000

    added rect elements

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-
 
 function tDiagram(dataset) {
 
-    console.log(dataset)
+    // console.log(dataset)
 
     d3.select('body')
         .append('h2')
@@ -18,6 +18,41 @@ function tDiagram(dataset) {
         .append('h3')
         .attr('id', 'description')
         .text('Description')
+
+    const treemap = dataset => d3.treemap()
+        .tile(d => d)
+        .size([100, 200])
+        .padding(1)
+        .round(true)
+        (d3.hierarchy(dataset)
+            .sum(d => d.value)
+            .sort((a, b) => b.value - a.value))
+
+    console.log(treemap)
+
+    const root = treemap(dataset);
+
+    const svg = d3.create('svg')
+        .attr('viewBox', [0, 0, 100, 200])
+
+    const leaf = d3.selectAll('g')
+        .data(root.leaves())
+        .join('g')
+        .attr('transform', d => `transform($(d.x0),$(d.y0)`)
+
+    leaf.append('rect')
+        // .attr('id', d => (d.leafUID = window.uid('leaf').id))
+        .attr('class', 'tile')
+        .attr('fill', 'lightBlue')
+        .attr('width', d => d.x1 - d.x0)
+        .attr('height', d => d.y1 - d.y0)
+
+
+
+
+    
+
+
 
 
 
