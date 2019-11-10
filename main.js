1064f01f8b8b8105042bc38c40c2fba771f0a9bf
antonio <ammj24@hotmail.com>
2019-11-10 16:01:15 +0000

    tile area is proportional to data-value

--- main.js
+++ main.js
@@ -12,18 +12,21 @@ function tDiagram(dataset) {
     d3.select('body')
         .append('h2')
         .attr('id', 'title')
-        .text('Title')
+        .text('Movie Sales')
 
     d3.select('body')
         .append('h3')
         .attr('id', 'description')
-        .text('Description')
+        .text('Most Sold Movies Grouped By Category')
+
+    const width = 800
+    const height = 400
 
     const treemap = dataset => d3.treemap()
         // .tile(d => {console.log(d)})
-        .size([400, 200])
+        .size([width, height])
         .padding(1)
-        .round(true)
+        // .round(true)
         (d3.hierarchy(dataset)
             .sum(d => d.value)
             .sort((a, b) => b.height - a.height || b.value - a.value))
@@ -42,33 +45,31 @@ function tDiagram(dataset) {
 
     const svg = d3.select('body')
         .append('svg')
-        // .attr('viewBox', [0, 0, 400, 200])
-        .attr('width', 400)
-        .attr('height', 200)
+        .attr('width', width)
+        .attr('height', height)
 
     const leaf = svg.selectAll('g')
         .data(root.leaves())
         .join('g')
         .attr('transform', d => `translate(${d.x0},${d.y0})`)
-        // .attr('transform', d => {
-        //     console.log(d)
-        //     // return d;
-        // })
 
     leaf.append('rect')
         // .attr('id', d => (d.leafUID = window.uid('leaf').id))
         .attr('class', 'tile')
         .style('position', 'relative')
         .attr('fill', 'blue')
-        .attr('data-name', d => d['name'])
-        .attr('data-category', d => d['category'])
-        .attr('data-value', d => d['value'])
+        .attr('data-name', d => d['data']['name'])
+        .attr('data-category', d => d['data']['category'])
+        .attr('data-value', d => {
+            console.log(d)
+            return d['data']['value']
+        })
         .attr('width', d => d.x1 - d.x0)
         .attr('height', d => d.y1 - d.y0)
         .on('mouseover', d => {
             tooltip.style('visibility', 'visible')
-            console.log(d['value'])
-            tooltip.attr('data-value', d['value'])
+            // console.log(d['value'])
+            tooltip.attr('data-value', d['data']['value'])
         })
         .on('mouseout', d => tooltip.style('visibility', 'hidden'))
 
