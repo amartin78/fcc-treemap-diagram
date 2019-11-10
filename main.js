232cb6f3b194ffa93a6794b95683c11325bbf1ad
antonio <ammj24@hotmail.com>
2019-11-10 02:31:56 +0000

    added tooltip and data-value attribute

--- main.js
+++ main.js
@@ -20,15 +20,23 @@ function tDiagram(dataset) {
         .text('Description')
 
     const treemap = dataset => d3.treemap()
-        .tile(d => d)
+        // .tile(d => {console.log(d)})
         .size([400, 200])
         .padding(1)
         .round(true)
         (d3.hierarchy(dataset)
             .sum(d => d.value)
-            .sort((a, b) => b.value - a.value))
+            .sort((a, b) => b.height - a.height || b.value - a.value))
 
-    console.log(treemap)
+    // console.log(treemap)
+
+    const tooltip = d3.select('body')
+        .append('div')
+        .attr('id', 'tooltip')
+        .style('position', 'absolute')
+        .attr('width', d => 100)
+        .attr('height', 200)
+        .style('visibility', 'hidden')
     
     const root = treemap(dataset);
 
@@ -41,19 +49,26 @@ function tDiagram(dataset) {
     const leaf = svg.selectAll('g')
         .data(root.leaves())
         .join('g')
-        // .attr('transform', d => `transform($(d.x0),$(d.y0)`)
-        .attr('transform', d => {
-            console.log(d)
-        })
+        .attr('transform', d => `translate(${d.x0},${d.y0})`)
+        // .attr('transform', d => {
+        //     console.log(d)
+        //     // return d;
+        // })
 
     leaf.append('rect')
         // .attr('id', d => (d.leafUID = window.uid('leaf').id))
         .attr('class', 'tile')
+        .style('position', 'relative')
         .attr('fill', 'blue')
+        .attr('data-name', d => d['name'])
+        .attr('data-category', d => d['category'])
+        .attr('data-value', d => d['value'])
         .attr('width', d => d.x1 - d.x0)
         .attr('height', d => d.y1 - d.y0)
         .on('mouseover', d => {
             tooltip.style('visibility', 'visible')
+            console.log(d['value'])
+            tooltip.attr('data-value', d['value'])
         })
         .on('mouseout', d => tooltip.style('visibility', 'hidden'))
 
